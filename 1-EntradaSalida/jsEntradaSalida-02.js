8b3b96ad48a314139e7aa2be5c152ccfd78a16a9
HectorSantiagoEsquivel <hectorsantiagoesquivel@gmail.com>
2022-02-03 16:39:03 -0300

    Primer Cambio

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,14 @@
 /*
+Héctor Santiago Esquivel
+Ejercicio 2
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+
+	nombre=prompt("Escribi tu nombre");
+
+	alert(nombre);
 }
 
