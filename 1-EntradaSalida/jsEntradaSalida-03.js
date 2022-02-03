a06c9eba34735ab768d915bf335f22b17c11123f
HectorSantiagoEsquivel <hectorsantiagoesquivel@gmail.com>
2022-02-03 18:19:13 -0300

    Segundo Cambio

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,15 @@
 /*
+Hector Santiago Esquivel
+Ejercicio 3
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert(txtIdNombre.value);
+	let nombreIngresado;
+
+	nombreIngresado=document.getElementById("txtIdNombre").value;
+
+	alert(nombreIngresado);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,16 @@
 /* 
+Hector Santiago Esquivel
+Ejercicio 4
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombreIngresado;
+
+	nombreIngresado=prompt("Tu mensaje");
+	
+	document.getElementById("txtIdNombre").value=nombreIngresado;
+
 }
 
